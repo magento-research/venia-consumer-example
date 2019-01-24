c91d0bb469124845fb5f2538bb7e70371c2d84bd
zetlen <jzetlen@adobe.com>
2019-01-24 13:58:09 -0600

    WIP trying to do a venia dependency

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import VeniaProductDetail from '@magento/venia-concept/es2015/components/ProductFullDetail';
+import { Notification } from 'react-notification'
 
 class App extends Component {
+  state = {}
+  componentDidMount() {
+    this.getProduct('https://gist.githubusercontent.com/zetlen/0b27881fe7dd26b5c79cfdf548043ad1/raw/dd16d1533fbfa1e84f9e3d85e75c3efb2c8ba640/silver-amor-bangle-set.json');
+  }
+  async getProduct(url) {
+    const res = await fetch(url);
+    this.setState({ product: await res.json() });
+  }
   render() {
+    const { product, added } = this.state;
+    const notification = added ? <Notification message={`Added ${added} to cart!`} action="Cool" dismissAfter={3000} onDismiss={() => this.setState({ added: false })} /> : null;
+    const productDetail = product ? <VeniaProductDetail
+        product={product} addToCart={() => this.setState({ added: product.name })} /> : null;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
+          <p> Oh look, it's a <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://facebook.github.io/create-react-app/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
-          </a>
+            create-react-app
+          </a> starter using a Venia component.</p>
         </header>
+        {notification}
+        {productDetail}
       </div>
     );
   }
